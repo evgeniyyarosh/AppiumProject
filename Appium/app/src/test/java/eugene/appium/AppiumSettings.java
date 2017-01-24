2f4e3459615fac33c0b42e02dfab3a4446201e43
eugeneiarosh-pc\eugene.iarosh <evgeniy.iarosh@gmail.com>
2017-01-24 11:48:49 +0200

    First test Login

--- Appium/app/src/test/java/eugene/appium/AppiumSettings.java
+++ Appium/app/src/test/java/eugene/appium/AppiumSettings.java
@@ -16,16 +16,14 @@ public class AppiumSettings {
 
     private static AndroidDriver wd;
     private static final String TESTDROID_SERVER = "http://127.0.0.1";
-    public static String app = "D:/BusAPK/school_bus_app_1.0.1-debug-dev-f94349a.apk";
-    public static String appPackage = "com.smartnavigationsystems.ourschoolbus";
-    public static String activity = "com.smartnavigationsystems.ourschoolbus.presentation.views.activities.SplashActivity_";   // под вопросом
+    public static String app = "D:/BusAPK/school_bus_app_1.0.1-debug-dev-f94349a.apk";  //set the link to app
+    public static String appPackage = "com.smartnavigationsystems.ourschoolbus"; // Set your application's appPackage
+    public static String activity = "com.smartnavigationsystems.ourschoolbus.presentation.views.activities.SplashActivity_";
     public static String device = "Android";
-    public static String deviceName = "IVGAUGQGRSEESKY9";
-    public static String platformVersion = "4.4.2";
-    public static String platformName = "Android";
+    public static String deviceName = "IVGAUGQGRSEESKY9";  //my devise name
+    public static String platformVersion = "4.4.2";  //set mobile device OS version
+    public static String platformName = "Android";  // set device platform
 
-
-//test
 
     public static AndroidDriver setUp() throws MalformedURLException {
         DesiredCapabilities dc = new DesiredCapabilities();
@@ -37,6 +35,10 @@ public class AppiumSettings {
         dc.setCapability("app-package", appPackage);
         dc.setCapability("app-activity", activity);
 
+
+
+        // Created object of RemoteWebDriver will all set capabilities.
+        // Set appium server address and port number in URL string.
         wd = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), dc);
 
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
--- Appium/app/src/test/java/eugene/appium/LoginFirstTest.java
+++ Appium/app/src/test/java/eugene/appium/LoginFirstTest.java
@@ -1,8 +1,41 @@
 package eugene.appium;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+
 /**
  * Created by eugene.iarosh on 1/24/2017.
  */
 
 public class LoginFirstTest {
+    private static AndroidDriver wd;
+    private static final String BASEPATH = "com.smartnavigationsystems.ourschoolbus:id/";
+
+    @BeforeClass
+    public static void appiumInit(){
+        try{
+            wd = AppiumSettings.setUp();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void loginApp(){
+        wd.findElement(By.id(BASEPATH + "phone_number_edit_text")).sendKeys("963963");
+        wd.findElement(By.id(BASEPATH + "password_text_input_edit_text")).sendKeys("qweqwe");
+        wd.findElement(By.id(BASEPATH + "login_button")).click();
+        wd.findElement(By.name("Map")).isDisplayed();
+    }
+
+    /*@AfterClass
+    public static void signOut(){
+        AppiumSettings.tearDown();
+    }*/
 }
